da5cf2ad339e9097e3c4c880dd49e305a585b937
gschles <gschles@cs.stanford.edu>
2013-03-05 10:55:23 -0800

    Added parse tree info to auxillary token class.

--- src/dataclasses/AuxTokenInfo.java
+++ src/dataclasses/AuxTokenInfo.java
@@ -1,15 +1,20 @@
 package dataclasses;
 
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.trees.Tree;
 
 public class AuxTokenInfo {
 	public int tokenOffset;
 	public CoreLabel prev;
 	public CoreLabel next;
+	public Tree tree;
+	public int tree_idx;
 	
 	public AuxTokenInfo() {
 		tokenOffset = -1;
 		prev = null;
 		next = null;
+		tree = null;
+		tree_idx = -1;
 	}
 }
