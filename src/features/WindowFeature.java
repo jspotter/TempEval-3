bcf8451c3b2a74b84a180a5896fbcd63dcedad37
jspotter <potterjs61490@gmail.com>
2013-03-04 20:58:05 -0800

    Basic same-sentence event relationship tagging.

---
+++ src/features/WindowFeature.java
@@ -0,0 +1,93 @@
+package features;
+
+import java.util.List;
+
+import annotationclasses.AuxTokenInfoAnnotation;
+import annotationclasses.EventAnnotation;
+
+import dataclasses.AuxTokenInfo;
+import dataclasses.EventInfo;
+
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+
+public class WindowFeature implements TempEvalFeature {
+	
+	private static final String LEFT_OF_FIRST = "__LEFT_OF_FIRST__";
+	private static final String RIGHT_OF_FIRST = "__RIGHT_OF_FIRST__";
+	private static final String LEFT_OF_SECOND = "__LEFT_OF_SECOND__";
+	private static final String RIGHT_OF_SECOND = "__RIGHT_OF_SECOND__";
+	private static final String NEAR_FIRST = "__NEAR_FIRST__";
+	private static final String NEAR_SECOND = "__NEAR_SECOND__";
+	private static final String NEAR_SOMEONE = "__NEAR_SOMEONE__";
+	
+	private static int nextID = 0;
+	private int id;
+	
+	/*
+	 * Number of words examined in either direction.
+	 */
+	private int windowSize = 0;
+	private Class infoClass = TextAnnotation.class;
+	
+	public WindowFeature(int size, Class infoClass) {
+		windowSize = size;
+		this.infoClass = infoClass;
+		this.id = nextID++;
+	}
+	
+	public void setWindowSize(int size) {
+		windowSize = size;
+	}
+	
+	public int getWindowSize() {
+		return windowSize;
+	}
+
+	@Override
+	public void add(List<String> features, CoreLabel token1, CoreLabel token2) {
+		
+		AuxTokenInfo aux1 = token1.get(AuxTokenInfoAnnotation.class);
+		AuxTokenInfo aux2 = token2.get(AuxTokenInfoAnnotation.class);
+		
+		CoreLabel left1 = aux1.prev;
+		CoreLabel right1 = aux1.next;
+		CoreLabel left2 = aux2.prev;
+		CoreLabel right2 = aux2.next;
+		
+		for (int i = 0; i < windowSize; i++) {
+			if (left1 != null) {
+				String value = left1.get(infoClass);
+				features.add(LEFT_OF_FIRST + id + value + "=TRUE");
+				features.add(NEAR_FIRST + id + value + "=TRUE");
+				features.add(NEAR_SOMEONE + id + value + "=TRUE");
+				left1 = left1.get(AuxTokenInfoAnnotation.class).prev;
+			}
+			
+			if (right1 != null) {
+				String value = right1.get(infoClass);
+				features.add(RIGHT_OF_FIRST + id + value + "=TRUE");
+				features.add(NEAR_FIRST + id + value + "=TRUE");
+				features.add(NEAR_SOMEONE + id + value + "=TRUE");
+				right1 = right1.get(AuxTokenInfoAnnotation.class).next;
+			}
+			
+			if (left2 != null) {
+				String value = left2.get(infoClass);
+				features.add(LEFT_OF_SECOND + id + value + "=TRUE");
+				features.add(NEAR_SECOND + id + value + "=TRUE");
+				features.add(NEAR_SOMEONE + id + value + "=TRUE");
+				left2 = left2.get(AuxTokenInfoAnnotation.class).prev;
+			}
+			
+			if (right2 != null) {
+				String value = right2.get(infoClass);
+				features.add(RIGHT_OF_SECOND + id + value + "=TRUE");
+				features.add(NEAR_SECOND + id + value + "=TRUE");
+				features.add(NEAR_SOMEONE + id + value + "=TRUE");
+				right2 = right2.get(AuxTokenInfoAnnotation.class).next;
+			}
+		}
+	}
+
+}
--- src/tempeval/SameSentenceEventTagger.java
+++ src/tempeval/SameSentenceEventTagger.java
@@ -9,6 +9,7 @@ import edu.stanford.nlp.classify.*;
 import edu.stanford.nlp.io.IOUtils;
 import edu.stanford.nlp.ling.BasicDatum;
 import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
@@ -16,6 +17,14 @@ import edu.stanford.nlp.ling.Datum;
 import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.util.CoreMap;
 import edu.stanford.nlp.util.Pair;
+import features.DistanceFeature;
+import features.EventLemmaFeature;
+import features.EventTypeFeature;
+import features.InterleavingCommaFeature;
+import features.POSFeature;
+import features.TempEvalFeature;
+import features.TimexTypeFeature;
+import features.WindowFeature;
 
 import org.w3c.dom.*;
 
@@ -36,46 +45,208 @@ public class SameSentenceEventTagger {
 	private static final String CLASSIFIER_FILENAME = 
 			"classifiers/same-sentence-event-model.ser.gz";
 	
-	private static LinearClassifier<String, String> trainClassifier, testClassifier;
-	private static LinearClassifierFactory<String, String> factory;
-	private static List<Datum<String, String>> trainingData;
+	private double truePositives = 0, falsePositives = 0, falseNegatives = 0;
+	
+	private LinearClassifier<String, String> trainClassifier, testClassifier;
+	private LinearClassifierFactory<String, String> factory;
+	private List<Datum<String, String>> trainingData;
+	private List<TempEvalFeature> featureList;
 
 	public SameSentenceEventTagger() {
-		factory = new LinearClassifierFactory<String, String>(); // jp
+		factory = new LinearClassifierFactory<String, String>();
 		factory.useConjugateGradientAscent();
 		factory.setVerbose(true);
 		factory.setSigma(10.0);
 
 		trainingData = new ArrayList<Datum<String, String>>();
+		
+		featureList = new ArrayList<TempEvalFeature>();
+		featureList.add(new DistanceFeature());
+		featureList.add(new InterleavingCommaFeature());
+		featureList.add(new WindowFeature(3, TextAnnotation.class));
+		featureList.add(new EventTypeFeature());
+		featureList.add(new POSFeature());
+		featureList.add(new EventLemmaFeature());
+		featureList.add(new WindowFeature(2, PartOfSpeechAnnotation.class));
 	}
 	
 	/*
-	 * TODO
+	 * Get all same-sentence event pairs for a document. First element of each pair
+	 * occurs before second element in the sentence.
+	 */
+	private static Set<Pair<CoreLabel, CoreLabel>> getEventPairs(Annotation annotation) {
+		Set<Pair<CoreLabel, CoreLabel>> result = new HashSet<Pair<CoreLabel, CoreLabel>>();
+		List<CoreMap> sentences = annotation.get(CoreAnnotations.SentencesAnnotation.class);
+
+		// For each sentence, find all timexes and events, and then make pairs
+		for(CoreMap sentence: sentences) {
+
+			List<CoreLabel> tokens = sentence.get(TokensAnnotation.class);
+
+			// Keep track of all events in this sentence
+			List<CoreLabel> events = new ArrayList<CoreLabel>();
+
+			// Keep track of last eid (for multi-token events)
+			String lastEid = null;
+
+			// Iterate through tokens and store all events and timexes. Relies on the
+			// invariant that no token will be both an event and a timex.
+			for (CoreLabel token: tokens) {
+				EventInfo eventInfo = token.get(EventAnnotation.class);
+				
+				// Handle event tokens
+				if (eventInfo != null) {
+
+					// Check for multiple tokens in same event tag
+					if (lastEid != null && lastEid.equals(eventInfo.currEventId))
+						continue;
+					lastEid = eventInfo.currEventId;
+
+					events.add(token);
+				} else {
+					lastEid = null;
+				}
+			}
+
+			// Make pairs for this sentence
+			for (int i = 0; i < events.size(); i++) {
+				for (int j = i + 1; j < events.size(); j++) {
+					result.add(new Pair<CoreLabel, CoreLabel>(events.get(i), events.get(j)));
+				}
+			}
+		}
+
+		return result;
+	}
+
+	/*
+	 * Get true same-sentence relationships between events from
+	 * the training data. Key string is eiid of first event. Second key string is
+	 * eiid of second event. Final value is relationship type.
+	 */
+	private static Map<String, Map<String, String>> getEventRelationships(Document doc) {
+		Map<String, Map<String, String>> result = new HashMap<String, Map<String, String>>();
+
+		Element root = doc.getDocumentElement();
+		Element[] tlinkElems = 
+				XMLParser.getElementsByTagNameNR(root, "TLINK");
+		for(Element e : tlinkElems) {
+			String eventInstanceID = e.getAttribute("eventInstanceID");
+			String relatedToEventInstance = e.getAttribute("relatedToEventInstance");
+			String relType = e.getAttribute("relType");
+
+			if (eventInstanceID.length() > 0 && relatedToEventInstance.length() > 0
+					&& relType.length() > 0) {
+				MapUtils.doublePut(result, eventInstanceID, relatedToEventInstance, relType);
+			}
+		}
+
+		return result;
+	}
+	
+	/*
+	 * Creates a single datum from a training example
+	 */
+	private Datum<String, String> getDatum(CoreLabel eventToken1, CoreLabel eventToken2,
+			Set<Pair<CoreLabel, CoreLabel>> pairs, Map<String, Map<String, String>> relationships) {
+
+		List<String> features = new ArrayList<String>();
+
+		EventInfo eventInfo1 = eventToken1.get(EventAnnotation.class);
+		EventInfo eventInfo2 = eventToken2.get(EventAnnotation.class);
+
+		// FEATURES
+		for (TempEvalFeature feature: featureList) {
+			feature.add(features, eventToken1, eventToken2);
+		}
+
+		// LABEL
+		String label = MapUtils.doubleGet(relationships, eventInfo1.currEiid, 
+				eventInfo2.currEiid);
+		label = (label == null ? "O" : label);
+		//System.out.println(label);
+
+		return new BasicDatum<String, String>(features, label);
+	}
+	
+	/*
+	 * Train classifier on relationships between same-sentence timexes and events.
 	 */
 	public void train(Annotation annotation, Document doc) {
-		
-		
-	}
-	
-	/*
-	 * TODO
-	 */
-	public void test(Annotation annotation, Document doc) {
+		// Find all possible same-sentence event pairs
+		// Extract JUST the links between events from parsed XML (doc)
+		Set<Pair<CoreLabel, CoreLabel>> pairs = getEventPairs(annotation);
+		Map<String, Map<String, String>> relationships = getEventRelationships(doc);
 
-
+		for (Pair<CoreLabel, CoreLabel> pair: pairs) {
+			trainingData.add(getDatum(pair.first, pair.second, pairs, relationships));
+		}
 	}
 	
 	/*
-	 * TODO
-	 */
-	public void loadTestClassifier() {
-		
-	}
-	
-	/*
-	 * TODO
+	 * Zips classifier into file
 	 */
 	public void doneClassifying() {
+		trainClassifier = factory.trainClassifier(trainingData);
+		LinearClassifier.writeClassifier(trainClassifier, CLASSIFIER_FILENAME);
+	}
 
+	/*
+	 * Loads test classifier from file (presumably written from training)
+	 */
+	public void loadTestClassifier() {
+		testClassifier = LinearClassifier.readClassifier(CLASSIFIER_FILENAME);
+	}
+	
+	/*
+	 * Tests classifier
+	 */
+	public void test(Annotation annotation, Document doc) {
+		DocInfo docInfo = annotation.get(DocInfoAnnotation.class);
+
+		// Find all possible same-sentence timex event pairs
+		// Extract JUST the links between events and timex's from parsed XML (doc)
+		Set<Pair<CoreLabel, CoreLabel>> pairs = getEventPairs(annotation);
+		Map<String, Map<String, String>> relationships = getEventRelationships(doc);
+
+		for (Pair<CoreLabel, CoreLabel> pair: pairs) {
+			Datum<String, String> datum = getDatum(pair.first, pair.second, pairs, relationships);
+			String guess = testClassifier.classOf(datum);
+			String correct = datum.label();
+
+			System.out.println(pair.first.get(EventAnnotation.class).currEiid + " "
+					+ pair.second.get(EventAnnotation.class).currEiid + " "
+					+ guess + " " + correct);
+			testClassifier.justificationOf(datum);
+
+			if (!guess.equals("O")) {
+				System.out.println("guessed something in file " + docInfo.filename);
+				EventInfo info1 = pair.first.get(EventAnnotation.class);
+				EventInfo info2 = pair.second.get(EventAnnotation.class);
+				LinkInfo link = new LinkInfo("-1", guess, null,
+						info1, info2);
+				pair.first.set(LinkInfoAnnotation.class, link);
+			}
+			
+			if (guess.equals(correct) && !guess.equals("O"))
+				truePositives++;
+			else if (guess.equals("O"))
+				falseNegatives++;
+			else
+				falsePositives++;
+		}
+	}
+	
+	public void printStats() {
+		System.out.println("Summary\n-----");
+		System.out.println("  TP: " + truePositives);
+		System.out.println("  FP: " + falsePositives);
+		System.out.println("  FN: " + falseNegatives);
+		System.out.println("  Pr: " + (truePositives / 
+				(truePositives + falsePositives)));
+		System.out.println("  Re: " + (truePositives / 
+				(truePositives + falseNegatives)));
+		System.out.println("  F1: " + ((2 * truePositives) / 
+				(truePositives + falsePositives + falseNegatives)));
 	}
 }
