20acd53aa436856e8ed5ac0c6f133a4f5f8dfc73
Justin Heermann <justin.heermann@gmail.com>
2013-03-09 18:36:54 -0800

    changed interval feature so that it will work with 2 arguements

--- src/features/IntervalFeature.java
+++ src/features/IntervalFeature.java
@@ -12,7 +12,8 @@ import edu.stanford.nlp.ling.CoreLabel;
 
 public class IntervalFeature implements TempEvalFeature {
 	
-	private static final String INTERVAL_STRING = "__INTER__";
+	private static final String FIRST_INTERVAL_STRING = "__FIRST_INTER__";
+	private static final String SECOND_INTERVAL_STRING = "__SECOND_INTER__";
 
 	private static final HashSet<String> interval_strings = new HashSet<String>(Arrays.asList(new String[] {"before","after","during","on","this","in","for","until","since","between","by","advance", "at", "to","from","while","over","when","prior","early","around","ahead","within"}));
 	private int prev_window_size;
@@ -24,18 +25,35 @@ public class IntervalFeature implements TempEvalFeature {
 	
 	@Override
 	public void add(List<String> features, CoreLabel token1, CoreLabel token2) {
-		AuxTokenInfo auxTimeInfo = token1.get(AuxTokenInfoAnnotation.class);
+		AuxTokenInfo first_auxTimeInfo = token1.get(AuxTokenInfoAnnotation.class);
+		CoreLabel first_prev = first_auxTimeInfo.prev;
 		
-		CoreLabel prev = auxTimeInfo.prev;
+		AuxTokenInfo second_auxTimeInfo = null;
+		CoreLabel second_prev = null;
+		if(token2 != null){
+			second_auxTimeInfo = token2.get(AuxTokenInfoAnnotation.class);
+			second_prev = second_auxTimeInfo.prev;
+		}
+		
+		
 		for(int i = 0; i < prev_window_size; i++){
 		
-			if (prev != null) {
+			if (first_prev != null) {
 				//System.out.println(prev.get(TextAnnotation.class));
-				String s = prev.get(TextAnnotation.class);
+				String s = first_prev.get(TextAnnotation.class);
 				if(interval_strings.contains(s.toLowerCase())){
-					features.add(INTERVAL_STRING + "=" + i + s.toLowerCase());
+					features.add(FIRST_INTERVAL_STRING + "=" + i + s.toLowerCase());
 				}
-				prev = prev.get(AuxTokenInfoAnnotation.class).prev;
+				first_prev = first_prev.get(AuxTokenInfoAnnotation.class).prev;
+			}
+			
+			if (second_prev != null) {
+				//System.out.println(prev.get(TextAnnotation.class));
+				String s = second_prev.get(TextAnnotation.class);
+				if(interval_strings.contains(s.toLowerCase())){
+					features.add(SECOND_INTERVAL_STRING + "=" + i + s.toLowerCase());
+				}
+				second_prev = second_prev.get(AuxTokenInfoAnnotation.class).prev;
 			}
 		}
 	}
